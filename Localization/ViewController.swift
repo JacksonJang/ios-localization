fbe7633f76d38c5c4a132353b94d4e5af5658a6f
장효원 <hyo961015@naver.com>
2024-04-01 21:02:08 +0900

    카탈로그와 Legacy 추가

--- Localization/ViewController.swift
+++ Localization/ViewController.swift
@@ -10,12 +10,25 @@ import UIKit
 class ViewController: UIViewController {
     // Xcode 15 and later
     @IBOutlet weak var xcode15Label: UILabel!
+    
     @IBOutlet weak var xcode15CodeLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        xcode15CodeLabel.text = NSLocalizedString("Xcode before code", comment: "이것은 코드에서 사용하는 방법입니다.")
+        xcode15Label.text = String(localized: "Xcode15 and later")
+        
+        // 이름이 Localizable_legacy가 아닌, Localizable 라면, 아래처럼 사용 가능
+//        xcode15CodeLabel.text = NSLocalizedString("Xcode before code", comment: "이것은 코드에서 사용하는 방법입니다.")
+        
+        xcode15CodeLabel.text = NSLocalizedString("Xcode before code", tableName: "Localizable_legacy", comment: "이것은 코드에서 사용하는 방법입니다.")
+        
+        let numberLocalized = String(localized: "\(123) Test")
+        print(numberLocalized)
+        let strLocalized = String(localized: "123 Test")
+        print(strLocalized)
+        
+        let anyText = String(localized: "아무거나 넣어도 나와요")
     }
 
 
